0e229300a43b0020a1df224e0842a4ea633b5b3b
0bipinnata0 <47297706+0bipinnata0@users.noreply.github.com>
2025-10-11 23:41:13 +0800

    chore: 移除cloudflare

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+// import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -7,4 +7,4 @@ const nextConfig: NextConfig = {
 
 export default nextConfig;
 
-initOpenNextCloudflareForDev();
+// initOpenNextCloudflareForDev();
